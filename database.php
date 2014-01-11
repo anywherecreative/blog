90c622c3d385c584f585858c7e0082934f785ac2
Jeff Manning <jeff.manning.1989@hushmail.com>
2014-01-11 15:11:32 -0500

    Added Postgresql support

--- database.php
+++ database.php
@@ -10,10 +10,13 @@
 			$conf = new Configuration();
 			$this->type = $conf->getDBType;
 			if($conf->getDBType == "mysql") {
-				$this->connection = new mysqli($conf->getDBHost,$ocnf->getDBuser(),$conf->getDBPass(),$conf->getDBDatabase());
+				$this->connection = new mysqli($conf->getDBHost(),$ocnf->getDBuser(),$conf->getDBPass(),$conf->getDBDatabase());
 				if ($mysqli->connect_error) {
 					 throw new Exception('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
 				}
+			}
+			if($this->type == "postgresql") {
+				$this->connection = pg_connect("host=" . $conf->getDBHost . " dbname=" . $conf->getDBDatabase() . " user=" . $ocnf->getDBuser() . " password=" . $conf->getDBPass());
 			}
 			$this->prefix = $conf->getDBPrefix();
 		}
@@ -38,6 +41,9 @@
 			if($this->type == "mysql") {
 				return $this->connection->real_escape_string($item);
 			}
+			if($this->type == "postgresql") {
+				pg_escape_string ($this->connection,$item);
+			}
 		}
 		
 		/**
@@ -48,15 +54,23 @@
 		public function query($sql,$pre='#__') {
 			//replace the generic prefix with proper table prefix
 			$sql = str_replace ($pre, $this->prefix, $sql); 
-			$this->result = $connection->query($sql);
+			if($this->type == "mysql") {
+				$this->result = $this->connection->query($sql);
+			}
+			if($this->type == "postgresql") {
+				$this->result = pg_query($sql);
+			}
 		}
 		/**
 		make a prepared statement to avoid SQL injection attacks
 		@param $sql String the sql query
-		@param $types the types for the sql statement
+		@param MySQL only $types the types for the sql statement
+		@param Postgresql only $types the name of the query
 		@param ... list of fields to be put into the query
 		**/
-		public function preparedQuery($sql, $types) {
+		public function preparedQuery($sql, $types) 
+			//replace the generic prefix with proper table prefix
+			$sql = str_replace ($pre, $this->prefix, $sql);  
 			if($this->type == "mysql") {
 				$args = func_get_args();
 				$stmt = $mysqli->prepare($sql);
@@ -66,7 +80,15 @@
 				$stmt->execute();
 				$this->result = $stmt->get_result();
 			}
+			if($this->type == "postgresql") {
+				$args = func_get_args();
+				pg_prepare($this->connection, $types, $sql);
+				for($a = 2; a < count($args);$a++)
+					$items[$a-2] = $stmt[$a];
+				$this->result = pg_execute($this->connection, $types, $items);
+			}
 		}
+		
 		/**
 		return an associative array from the result set
 		@return array an array of fields
@@ -74,6 +96,9 @@
 		public function fetchAssoc() {
 			if($this->type == "mysql") {
 				return $this->result->fetch_assoc();
+			}
+			if($this->type == "postgresql") {
+				return pg_fetch_assoc($this->result);
 			}
 		}
 	}
